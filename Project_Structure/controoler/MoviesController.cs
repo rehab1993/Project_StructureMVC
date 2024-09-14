280dede7cdcceeb873f6df7e457fc56fac34a852
Rehab <rehab123abdallah@gmail.com>
2024-09-14 11:56:08 +0300

    Session01Video02(ActionResult)

--- Project_Structure/controoler/MoviesController.cs
+++ Project_Structure/controoler/MoviesController.cs
@@ -4,13 +4,19 @@ namespace Project_Structure.controoler
 {
     public class MoviesController : Controller
     {
-        public void GetMovies(int id)
+        public IActionResult GetMovie(int id)
         {
+            ContentResult result = new ContentResult();
+            result.Content = $"Movies with id {id}";
+           // result.ContentType = "object/pdf";
+            return result;
             
         }
         public string Index()
         {
             return "Hello From Index";
         }
+
+        
     }
 }

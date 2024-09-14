9e9e3597c9cba94da66486fbfd285a7b60de1f9a
Rehab <rehab123abdallah@gmail.com>
2024-09-14 10:29:14 +0300

    CraetionOfDev

--- Project_Structure/Program.cs
+++ Project_Structure/Program.cs
@@ -13,6 +13,7 @@ namespace Project_Structure
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("hello from Dev");
             CreateHostBuilder(args).Build().Run();
         }
 
